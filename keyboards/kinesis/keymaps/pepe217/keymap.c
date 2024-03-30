1dd73918bd8fee8880f553782453e5c1d8a26386
pepe217 <michael.r.pepe@gmail.com>
2024-03-30 13:20:05 -0700

    Keymap updates

--- keyboards/kinesis/keymaps/pepe217/keymap.c
+++ keyboards/kinesis/keymaps/pepe217/keymap.c
@@ -13,14 +13,12 @@
 // shift key overrides
 const key_override_t comma_override = ko_make_basic(MOD_MASK_SHIFT, KC_COMM, KC_LPRN);
 const key_override_t dot_override = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, KC_RPRN);
-const key_override_t colon_override = ko_make_basic(MOD_MASK_SHIFT, KC_COLN, KC_SCLN);
-const key_override_t underscore_override = ko_make_basic(MOD_MASK_SHIFT, KC_UNDS, KC_MINS);
 const key_override_t lparen_override = ko_make_basic(MOD_MASK_SHIFT, KC_LPRN, KC_LABK);
 const key_override_t rparen_override = ko_make_basic(MOD_MASK_SHIFT, KC_RPRN, KC_RABK);
 
 
 const key_override_t **key_overrides = (const key_override_t *[]){
-    &comma_override, &dot_override, &colon_override, &underscore_override, &lparen_override, &rparen_override,
+    &comma_override, &dot_override, &lparen_override, &rparen_override,
     NULL // Null terminate the array of overrides!
 };
 
@@ -65,15 +63,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [ENGRAM] = LAYOUT(
-    KC_ESC,       KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,         KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SCRL,  KC_PAUS,  KC_NO,    QK_BOOT,
-    KC_GRV,       KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                                                      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     RGUI(KC_3),
-    KC_TAB,       KC_B,     KC_Y,     KC_O,     KC_U,     KC_QUOT,                                                                   KC_COLN,  KC_L,     KC_D,     KC_W,     KC_V,     KC_Z,
-    CW_TOGG,      MT(MOD_RGUI, KC_C),     MT(MOD_RALT, KC_I),     MT(MOD_RCTL, KC_E),     MT(MOD_LSFT, KC_A),     KC_COMM,          KC_DOT,   MT(MOD_LSFT, KC_H),     MT(MOD_RCTL, KC_T),     MT(MOD_RALT, KC_S),     MT(MOD_RGUI, KC_N),     KC_Q,
+    CW_TOGG,       KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,         KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SCRL,  KC_PAUS,  KC_NO,    QK_BOOT,
+    KC_EQL,       KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                                                      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     RGUI(KC_3),
+    KC_GRV,       KC_B,     KC_Y,     KC_O,     KC_U,     KC_QUOT,                                                                   KC_SCLN,  KC_L,     KC_D,     KC_W,     KC_V,     KC_Z,
+    KC_ESC,      MT(MOD_RGUI, KC_C),     MT(MOD_RALT, KC_I),     MT(MOD_RCTL, KC_E),     MT(MOD_LSFT, KC_A),     KC_COMM,          KC_DOT,   MT(MOD_LSFT, KC_H),     MT(MOD_RCTL, KC_T),     MT(MOD_RALT, KC_S),     MT(MOD_RGUI, KC_N),     KC_Q,
     RALT(KC_TAB), KC_G,     KC_X,     KC_J,     KC_V,     KC_UNDS,                                                                   KC_SLSH,  KC_R,     KC_M,     KC_F,     KC_P,     RGUI(KC_2),
-                  KC_BSLS,  KC_LBRC,  KC_LPRN,  KC_ESC,                                                                                        KC_EQL,   KC_RPRN,  KC_RBRC,  KC_SCLN,  
+                  KC_BSLS,  KC_LBRC,  KC_LPRN,  KC_TAB,                                                                                        KC_UP,   KC_DOWN,  KC_LBRC,  KC_RBRC,  
                                                       LT(FUNCTION, KC_RGUI),  KC_RALT,                     KC_RGUI,  LT(SYMBOL, KC_RCTL),
-                                                                KC_HOME,                                               KC_PGUP,
-                                            LT(CURSOR, KC_BSPC),  LT(NUMPAD, KC_DEL),   KC_END,                                                KC_PGDN,  KC_ENTER, KC_SPC
+                                                                LT(CURSOR, KC_HOME),                                               KC_PGUP,
+                                             KC_BSPC,  KC_DEL,   LT(NUMPAD, KC_END),                                                KC_PGDN,  KC_ENTER, KC_SPC
   ),
 
   [SYMBOL] = LAYOUT(
