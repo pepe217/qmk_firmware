c8fe50c87dbeb6a5b07f1dd2fae3e32fd10ae1c1
pepe217 <michael.r.pepe@gmail.com>
2024-03-30 12:58:53 -0700

    Numpad update

--- keyboards/kinesis/keymaps/pepe217/keymap.c
+++ keyboards/kinesis/keymaps/pepe217/keymap.c
@@ -89,15 +89,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [NUMPAD] = LAYOUT(
-    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,         KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SCRL,  KC_PAUS,  KC_NO,    QK_BOOT,
-    KC_EQL,   KC_UNDS,  KC_D,     KC_E,     KC_F,     KC_5,                                                                      KC_CIRC,  KC_DLR,   KC_HASH,  KC_AT,    KC_EXLM,  KC_PIPE,
-    KC_TAB,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_T,                                                                      KC_PERC,  KC_P7,    KC_P8,    KC_P9,    KC_COLN,  KC_K,
-    CW_TOGG,  KC_RGUI,  KC_RALT,  KC_RCTL,  KC_RSFT,     KC_G,                                                                      KC_PLUS,  KC_P4,    KC_P5,    KC_P6,    KC_MINS,  KC_J,
-    KC_RSFT, RCTL(KC_Z),KC_X,     KC_C,     KC_V,     KC_B,                                                                      KC_ASTR,  KC_P1,    KC_P2,    KC_P3,    KC_SLSH,  KC_G,
-              KC_GRV,   KC_INS,   KC_LEFT,  KC_RGHT,                                                                                       KC_EQL,   KC_DOT,   KC_LPRN,  KC_RPRN,
-                                                      KC_RCTL,  KC_RALT,                                               KC_RGUI,  KC_RCTL,
-                                                                KC_HOME,                                               KC_PGUP,
-                                            KC_BSPC,  KC_DEL,   KC_END,                                                KC_PGDN,  KC_ENTER, KC_P0
+    KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,         KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,
+    KC_ESC,   KC_ENTER,    KC_SPC, KC_TAB, KC_DEL, KC_INS,                                                                      KC_CIRC,  KC_DLR,   KC_HASH,  KC_AT,    KC_EXLM,  KC_PIPE,
+    KC_TAB,   OSM(MOD_LSFT),    RCS(KC_Z),    RCTL(KC_Z),     KC_BSPC,    KC_A,                                                                      KC_PERC,  KC_P7,    KC_P8,    KC_P9,    KC_COLN,  KC_K,
+    KC_NO,  KC_RGUI,  KC_RALT,  KC_RCTL,  KC_RSFT,     KC_B,                                                                      KC_PLUS,  KC_P4,    KC_P5,    KC_P6,    KC_MINS,  KC_J,
+    KC_NO, KC_NO,KC_NO,     KC_NO,     KC_UNDS,     KC_C,                                                                      KC_ASTR,  KC_P1,    KC_P2,    KC_P3,    KC_SLSH,  KC_G,
+              KC_NO,   KC_NO,   KC_NO,  KC_NO,                                                                                       KC_EQL,   KC_DOT,   KC_LPRN,  KC_RPRN,
+                                                      KC_D,  KC_E,                                               KC_LT,  KC_GT,
+                                                                KC_F,                                               KC_COMM,
+                                            KC_BSPC,  KC_DEL,   KC_END,                                                KC_NO,  KC_DOT, KC_P0
   ),
 
   [FUNCTION] = LAYOUT(
