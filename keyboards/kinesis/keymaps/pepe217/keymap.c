0a15edbfb667f741abfbd5175d56f118f8aa8a87
pepe217 <michael.r.pepe@gmail.com>
2024-03-30 13:00:25 -0700

    Function layer

--- keyboards/kinesis/keymaps/pepe217/keymap.c
+++ keyboards/kinesis/keymaps/pepe217/keymap.c
@@ -102,8 +102,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [FUNCTION] = LAYOUT(
     KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,         KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,
-    KC_NO,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,                                                                      KC_NO,     KC_F7,     KC_F8,     KC_F9,     KC_F10,     KC_F13,
-    KC_NO,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,                                                                      KC_NO,     KC_F4,     KC_F5,     KC_F6,     KC_F11,     KC_F14,
+    KC_ESC,   KC_ENTER,    KC_SPC, KC_TAB, KC_DEL, KC_INS,                                                                      KC_NO,     KC_F7,     KC_F8,     KC_F9,     KC_F10,     KC_F13,
+    KC_NO,   OSM(MOD_LSFT),    RCS(KC_Z),    RCTL(KC_Z),     KC_BSPC,     KC_NO,                                                                      KC_NO,     KC_F4,     KC_F5,     KC_F6,     KC_F11,     KC_F14,
     CW_TOGG,  KC_RGUI,  KC_RALT,  KC_RCTL,  KC_RSFT,     KC_NO,                                                                      KC_NO,     KC_F1,     KC_F2,     KC_F3,     KC_F12,  KC_F15,
     KC_NO,  KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,                                                                      KC_NO,     KC_NO,     KC_NO,  KC_NO,   KC_NO,  KC_NO,
               KC_NO,   KC_NO,   KC_NO,  KC_NO,                                                                                       KC_NO,    KC_NO,  KC_NO,  KC_NO,
