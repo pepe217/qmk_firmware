545c3c9c31b710c32fdd114918353619b644ed84
pepe217 <michael.r.pepe@gmail.com>
2024-04-26 21:52:23 -0700

    Custom shift keys

--- keyboards/splitkb/kyria/keymaps/pepe217/keymap.c
+++ keyboards/splitkb/kyria/keymaps/pepe217/keymap.c
@@ -1,3 +1,4 @@
+#include "features/custom_shift_keys.h"
 #include QMK_KEYBOARD_H
 
 #define QWERTY 6
@@ -5,8 +6,8 @@
 #define NUMPAD 1
 #define SYMBOL 2
 #define FUNCTION 3
-#define CURSOR 4 
-#define MISC 5
+#define MISC 4
+#define CURSOR 5 
 
 bool is_alt_tab_active = false; // ADD this near the beginning of keymap.c
 bool is_clt_tab_active = false; // ADD this near the beginning of keymap.c
@@ -23,22 +24,22 @@ enum custom_keycodes {          // Make sure have the awesome keycode ready
   J,
   K,
   G,
+  PARENS,
 };
 
-
-// shift key overrides
-const key_override_t comma_override = ko_make_basic(MOD_MASK_SHIFT, KC_COMM, KC_LPRN);
-const key_override_t dot_override = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, KC_RPRN);
-// const key_override_t lparen_override = ko_make_basic(MOD_MASK_SHIFT, KC_LPRN, KC_LABK);
-// const key_override_t rparen_override = ko_make_basic(MOD_MASK_SHIFT, KC_RPRN, KC_RABK);
-
-
-const key_override_t **key_overrides = (const key_override_t *[]){
-    &comma_override, &dot_override,
-    NULL // Null terminate the array of overrides!
+const custom_shift_key_t custom_shift_keys[] = {
+  {KC_DOT , KC_RPRN}, // Shift . is )
+  {KC_COMM, KC_LPRN}, // Shift , is (
+  {KC_UNDS, KC_MINS }, // Shift _ is -
+  {KC_COLN, KC_SCLN}, // Shift : is ; 
+  {KC_LBRC, KC_LCBR}, // Shift [ is {
 };
+uint8_t NUM_CUSTOM_SHIFT_KEYS =
+    sizeof(custom_shift_keys) / sizeof(custom_shift_key_t);
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+	 // for custom shift keys
+	 if (!process_custom_shift_keys(keycode, record)) { return false; }
   switch (keycode) { // This will do most of the grunt work with the keycodes.
     case J:
 		if (record->event.pressed) {
@@ -61,6 +62,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case LARRW:
 		if (record->event.pressed) {
 		SEND_STRING(" -> ");
+		}
+		return false;
+    case PARENS:
+		if (record->event.pressed) {
+			SEND_STRING("()");
 		}
 		return false;
     case HOME:
@@ -169,19 +175,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[ENGRAM] = LAYOUT_split_3x6_5(
 		KC_DEL, KC_B, KC_Y, KC_O, KC_U, KC_QUOT,					 														 	 KC_ESC, KC_L, KC_D, KC_W, KC_V, KC_Z,
 		KC_SLSH, LGUI_T(KC_C), LALT_T(KC_I), LCTL_T(KC_E), LSFT_T(KC_A), KC_COMM,												 KC_DOT, RSFT_T(KC_H), RCTL_T(KC_T), RALT_T(KC_S), RGUI_T(KC_N), KC_Q,
-		KC_SCLN, KC_G, KC_X, KC_J, KC_K, KC_UNDS,								 TG(NUMPAD), ALT_TAB,		   CLT_TAB,OSL(SYMBOL), KC_COLN, KC_R, KC_M, KC_F, KC_P, KC_EQL,
+		KC_LBRC, KC_G, KC_X, KC_J, KC_K, KC_UNDS,								 TG(NUMPAD), ALT_TAB,		   CLT_TAB,OSL(SYMBOL), KC_COLN, KC_R, KC_M, KC_F, KC_P, KC_EQL,
 						     KC_LEFT, KC_RGHT, KC_BSPC, KC_TAB, OSL(FUNCTION), 					OSL(MISC), LT(MISC,KC_ENT), KC_SPC, KC_UP, KC_DOWN
 	),
 	[NUMPAD] = LAYOUT_split_3x6_5(
 		KC_DEL, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_CIRC,							KC_PERC, KC_7, KC_8, KC_9, KC_COLN, K,
 		KC_PIPE, KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_AMPR,				 		KC_PPLS, KC_4, KC_5, KC_6, KC_MINS, J,
 		KC_A, KC_B, KC_C, KC_D, KC_E, KC_F, TG(NUMPAD), KC_TILD,	KC_LT, KC_GT, KC_PAST, KC_1, KC_2, KC_3, KC_SLSH, G,
-						  KC_LEFT, KC_RGHT, KC_BSPC, KC_W, KC_B,    KC_COMM, KC_PENT, KC_P0, KC_PEQL, KC_PDOT
+						  KC_LEFT, KC_RGHT, KC_BSPC, KC_W, KC_B,    KC_COMM, KC_ENT, KC_0, KC_PEQL, KC_PDOT
 	),
 	[SYMBOL] = LAYOUT_split_3x6_5(
-		KC_EXLM, KC_LBRC, LARRW, KC_DQUO, KC_RBRC, KC_QUES, KC_ESC, KC_BSPC, KC_TAB, KC_SPC, KC_ENT, KC_NO,
-		KC_HASH, KC_CIRC, KC_EQL, KC_MINS, KC_DLR, KC_ASTR, OSM(MOD_LSFT), OSM(MOD_LCTL), OSM(MOD_LCTL|MOD_LSFT), OSM(MOD_LALT), OSM(MOD_LGUI), KC_NO,
-		KC_AT, KC_LT, KC_PIPE, HOME, KC_GT, KC_SLSH, KC_TILD, KC_BSLS, COPY_NEW_TAB, VIM_SAVE, KC_SCLN, KC_DEL, LSFT(KC_TAB), KC_LPRN, KC_RPRN, KC_EQL,
+		KC_EXLM, LARRW, KC_DQUO, KC_RBRC, KC_RCBR, KC_QUES, KC_ESC, KC_BSPC, KC_TAB, KC_SPC, KC_ENT, KC_NO,
+		KC_HASH, KC_CIRC, KC_EQL, KC_ASTR, KC_DLR, PARENS, OSM(MOD_LSFT), OSM(MOD_LCTL), OSM(MOD_LCTL|MOD_LSFT), OSM(MOD_LALT), OSM(MOD_LGUI), KC_NO,
+		KC_AT, KC_LT, KC_PIPE, HOME, KC_GT, KC_BSLS, KC_TILD, KC_SLSH, COPY_NEW_TAB, VIM_SAVE, KC_SCLN, KC_DEL, LSFT(KC_TAB), KC_LPRN, KC_RPRN, KC_EQL,
 								KC_AMPR, KC_LCBR, KC_RCBR, KC_PERC, KC_GRV, TG(SYMBOL), KC_NO, KC_NO, KC_NO, KC_NO
 	),
 	[FUNCTION] = LAYOUT_split_3x6_5(
@@ -190,16 +196,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, OSM(MOD_LCTL|MOD_LALT), KC_F1, KC_F2, KC_F3, KC_F12, OSM(MOD_LCTL|MOD_LSFT),
 							 KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, CW_TOGG, KC_PSCR, KC_VOLU, KC_VOLD
 	),
+	[MISC] = LAYOUT_split_3x6_5(
+		LGUI(KC_1), KC_9, KC_8, KC_7, KC_6, KC_5, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+		LGUI(KC_2), KC_3, KC_2, KC_1, KC_0, KC_4, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+		LGUI(KC_3), LCTL(KC_4), LCTL(KC_3), LCTL(KC_2), LCTL(KC_1), LCTL(KC_5), LSFT(KC_INS), KC_NO, COPY_NEW_TAB, VIM_SAVE, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, 
+		RCS(KC_C), RCS(KC_V), LCTL(KC_C), LCTL(KC_V), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
+	),
 	[CURSOR] = LAYOUT_split_3x6_5(
 		KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
 		KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
 		KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
 		KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
 	),
-	[MISC] = LAYOUT_split_3x6_5(
-		LGUI(KC_1), KC_9, KC_8, KC_7, KC_6, KC_5, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-		LGUI(KC_2), KC_3, KC_2, KC_1, KC_0, KC_4, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-		LGUI(KC_3), LCTL(KC_4), LCTL(KC_3), LCTL(KC_2), LCTL(KC_1), LCTL(KC_5), LSFT(KC_INS), KC_NO, COPY_NEW_TAB, VIM_SAVE, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, 
-		RCS(KC_C), RCS(KC_V), LCTL(KC_C), LCTL(KC_V), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
-	)
 };
--- keyboards/splitkb/kyria/rules.mk
+++ keyboards/splitkb/kyria/rules.mk
@@ -1,1 +1,2 @@
 DEFAULT_FOLDER = splitkb/kyria/rev3
+SRC += features/custom_shift_keys.c
