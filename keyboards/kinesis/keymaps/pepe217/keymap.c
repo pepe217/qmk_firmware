2769502d00151e75971a1eafe7e0e3afcd92b442
pepe217 <michael.r.pepe@gmail.com>
2024-08-12 21:13:51 -0700

    Some updates.

--- keyboards/kinesis/keymaps/pepe217/keymap.c
+++ keyboards/kinesis/keymaps/pepe217/keymap.c
@@ -67,7 +67,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [ENGRAM] = LAYOUT(
-    KC_ESC, OSM(MOD_LGUI),       LCTL(KC_B),    KC_PGUP,    KC_PGDN,    KC_F5,    LCTL(KC_B), ALT_TAB,     CLT_TAB,     NEXT_TAB,         LGUI(KC_1), LGUI(KC_2),   KC_PSCR, LGUI(KC_3),   TO(0),  TG(QWERTY),    QK_BOOT,
+    KC_ESC, KC_F1,       KC_F2,    KC_PGUP,    KC_PGDN,    KC_F5,    LCTL(KC_B), ALT_TAB,     CLT_TAB,     OSM(MOD_LGUI),         LGUI(KC_1), LGUI(KC_2),   LGUI(KC_3), LGUI(KC_4), KC_PSCR,   TO(0),  TG(QWERTY),    QK_BOOT,
     KC_GRV,   KC_4,          KC_3,          KC_2,          KC_1,          KC_5,     KC_9,     KC_0,          KC_6,          KC_7,          KC_8,          KC_BSLS,
     OSL(FUNCTION), KC_B,          KC_Y,          KC_O,          KC_U,   KC_QUOT,  KC_COLN,  KC_L,   KC_D,          KC_W,          KC_V,          KC_Z,
     KC_ESC,  KC_C,  KC_I,  KC_E,  KC_A,  KC_COMM,  KC_DOT,   KC_H,  KC_T,  KC_S,  KC_N,  KC_Q,
@@ -136,15 +136,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                              KC_NO,  KC_NO,   KC_NO,                                               KC_NO,  KC_NO, KC_NO
   ),
   [QWERTY] = LAYOUT(
-    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,         KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SCRL,  KC_PAUS,  TG(QWERTY),    QK_BOOT,
-    KC_EQL,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                                                      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
+    KC_ESC, KC_F1,       KC_F2,    KC_PGUP,    KC_PGDN,    KC_F5,    LCTL(KC_B), ALT_TAB,     CLT_TAB,     OSM(MOD_LGUI),         LGUI(KC_1), LGUI(KC_2),   LGUI(KC_3), LGUI(KC_4), KC_PSCR,   TO(0),  TG(QWERTY),    QK_BOOT,
+    KC_EQL,   KC_4,          KC_3,          KC_2,          KC_1,          KC_5,     KC_9,     KC_0,          KC_6,          KC_7,          KC_8,          KC_MINS,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                                                                      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
-    KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                                                      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+    KC_ESC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                                                      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                                                                      KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
               KC_GRV,   KC_INS,   KC_LEFT,  KC_RGHT,                                                                                       KC_UP,    KC_DOWN,  KC_LBRC,  KC_RBRC,
-                                                      KC_LCTL,  KC_LALT,                                               KC_RGUI,  KC_RCTL,
-                                                                KC_HOME,                                               KC_PGUP,
-                                            KC_BSPC,  KC_DEL,   KC_END,                                                KC_PGDN,  KC_ENTER, KC_SPC
+                                                      OSM(MOD_LSFT),  OSM(MOD_LCTL),                                             OSM(MOD_RCTL),  OSM(MOD_RSFT),
+                                                                OSM(MOD_LALT),                                               OSM(MOD_RALT),
+                                             KC_BSPC,  KC_TAB,   TG(NUMPAD),                                               QK_REP,  KC_ENT, LT(MISC, KC_SPC)
   ),
 
   // [FUNCTION] = LAYOUT(
