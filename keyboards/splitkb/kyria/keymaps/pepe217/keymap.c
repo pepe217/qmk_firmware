649cb4c58ce5093f3bed472e858c2d8b29e4abe4
pepe217 <michael.r.pepe@gmail.com>
2025-05-18 12:33:14 -0700

    RGB

--- keyboards/splitkb/kyria/keymaps/pepe217/keymap.c
+++ keyboards/splitkb/kyria/keymaps/pepe217/keymap.c
@@ -3,6 +3,42 @@
 
 // tmux leader key
 #define TMUX_L  LCTL(KC_B)
+#define NUM_LEDS 62
+
+void keyboard_post_init_user(void) {
+  // Call the post init code.
+  rgblight_sethsv_range (HSV_BLUE, 0, NUM_LEDS);
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch (get_highest_layer(state)) {
+    case ENGRAM:
+        rgblight_sethsv_range (HSV_BLUE, 0, NUM_LEDS);
+        break;
+    case NUMPAD:
+        rgblight_sethsv_range (HSV_GREEN, 0, NUM_LEDS);
+        break;
+    case SYMBOL:
+        rgblight_sethsv_range (HSV_RED, 0, NUM_LEDS);
+        break;
+    case FUNCTION:
+        rgblight_sethsv_range (HSV_WHITE, 0, NUM_LEDS);
+        break;
+    case CURSOR:
+        rgblight_sethsv_range (HSV_ORANGE, 0, NUM_LEDS);
+        break;
+    case MAC_ENGRAM:
+        rgblight_sethsv_range (0x00,  0x00, 0x00, 0, NUM_LEDS);
+        break;
+    case QWERTY:
+        rgblight_sethsv_range (0x00,  0x00, 0x00, 0, NUM_LEDS);
+        break;
+    default: //  for any other layers, or the default layer
+        rgblight_sethsv_range (0x00,  0x00, 0x00, 0, NUM_LEDS);
+        break;
+    }
+  return state;
+}
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [ENGRAM]= LAYOUT(
@@ -12,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LPRN, KC_RPRN, LT(CURSOR, KC_SPC), LT(NUMPAD, KC_TAB), CW_TOGG,   KC_DEL , KC_BSPC ,LT(MOUSE, KC_R), KC_PGUP, KC_PGDN
     ),
     [SYMBOL] = LAYOUT(
-        KC_LBRC, KC_EXLM, KC_LCBR, KC_RCBR, KC_COMM, KC_QUES,                 KC_GRV, KC_DEL, LSFT(KC_TAB), KC_INS, KC_ESC, KC_NO,
+        KC_GRAVE, KC_EXLM, KC_LCBR, KC_RCBR, KC_COMM, KC_QUES,                 KC_GRV, KC_DEL, LSFT(KC_TAB), KC_INS, KC_ESC, KC_NO,
         KC_HASH, KC_CIRC, KC_LBRC, KC_RBRC, KC_DLR, KC_ASTR,                  KC_DQT, KC_BSPC, KC_TAB, KC_SPACE, KC_ENTER, KC_NO,
         KC_TILDE, KC_LT, KC_PIPE, KC_MINUS, KC_GT, KC_SLSH, KC_BSLS, KC_DOT,    KC_NO   ,KC_NO   ,KC_NO   ,KC_QUOT, KC_NO, KC_NO, KC_NO, KC_TRNS,
                 KC_AMPR  ,KC_PLUS   ,KC_PERC   ,KC_SCLN   ,KC_AT   ,               KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO
